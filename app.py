9f33ef7e3d62007ac4b6c614cf90cecf53260c75
John Eastman <jp.robotics@gmail.com>
2024-06-07 17:34:07 -0600

    add endpoint for saving lists

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, request
+import os
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/lists", methods=["GET", "POST"])
+def lists():
+    project_folder = os.path.expanduser(app.root_path)
+    file_path = os.path.join(project_folder, "data.json")
+
+    if request.method == "GET":
+        with open(file_path, "r") as file:
+            file_content = file.read()
+            return json.loads(file_content)
+
+    # Save Data
+    with open(file_path, "w") as file:
+        file_content = json.dumps(request.json)
+        file.write(file_content)
+
+    return {"message": "OK"}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
